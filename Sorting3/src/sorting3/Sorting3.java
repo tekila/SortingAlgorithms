0f401cbb911bd10efe7c62019d45c49d489f3025
Pedro <pedro.gaiarsa@users.noreply.github.com>
2018-03-24 12:36:46 -0300

    implemented quicksort

--- Sorting3/src/sorting3/Sorting3.java
+++ Sorting3/src/sorting3/Sorting3.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package sorting3;
+//package sorting3;
 
 import java.util.Scanner;
 
@@ -14,24 +14,62 @@ import java.util.Scanner;
 
 class Dado
 {
-    private int chave;
+    private String nome;//ascendente
+    private String cor; //ascendente
+    private String tamanho;//descendente
 
-    public Dado(int chave) {
-        this.chave = chave;
-    }
-    
-    public int getChave() {
-        return chave;
+    public Dado(String nome, String cor, String tamanho) {
+        this.nome = nome;
+        this.cor = cor;
+        this.tamanho = tamanho;
     }
 
-    public void setChave(int chave) {
-        this.chave = chave;
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public String getTamanho() {
+        return tamanho;
+    }
+
+    public void setTamanho(String tamanho) {
+        this.tamanho = tamanho;
     }
     
     
     public boolean ehMenorQue(Dado a)    
     {
-        return this.chave < a.getChave();
+        if(this.cor.compareTo(a.getCor()) == 0 )
+        {//mesmas cores, precisa desempatar
+            if(this.tamanho.compareTo(a.getTamanho()) == 0)
+            {//mesmos tamanhos, precisa desempatar
+                if(this.nome.compareTo(a.nome) < 0) return true; //this.nome vem antes de a.nome
+                else return false;
+            }else if(this.tamanho.compareTo(a.getTamanho()) > 0) //g<m<p 
+            {
+                return true;
+            } else return false;
+        } else if(this.cor.compareTo(a.getCor()) < 0)
+        {
+            return true;
+        } else return false;
+    }
+    
+    public void print()
+    {
+            System.out.println(this.cor + " " + this.tamanho + " " + this.nome);
     }
 }
 
@@ -44,7 +82,7 @@ class QuickSort
     }
     private void quickSort(Dado[] dados, int p, int r)
     {
-        if(p>r)
+        if(p<r)
         {
             int q = particiona(dados, p, r);
             quickSort(dados, p, q-1);
@@ -68,17 +106,17 @@ class QuickSort
     {
         Dado aux = dados[r];
         int i, j;
-        i = p-1;
+        i = p;
         for(j = p; j<r; j++)
         {
-            if(dados[i].ehMenorQue(aux))
+            if(dados[j].ehMenorQue(aux))
             {
-                i++;
                 swap(dados, i, j);
+                i++;
             }
         }
-        swap(dados, i+1, r-1);
-        return i+1;
+        swap(dados, i, r);
+        return i;
     }
     public void sort(Dado[] dados)
     {
@@ -99,11 +137,17 @@ public class Sorting3 {
         Dado [] dados = new Dado[n];
         for (int i = 0; i < dados.length; i++) {
          
-            dados[i] = new Dado(sc.nextInt());
+            dados[i] = new Dado(sc.next(), sc.next(), sc.next());
         }
         
-        QuickSort is = new QuickSort(dados);
-        is.sort(dados);
+        QuickSort qs = new QuickSort(dados);
+        qs.sort(dados);
+        
+        for (int i = 0; i < dados.length; i++) {
+         
+            dados[i].print();
+        }
+        
         
     }
     
